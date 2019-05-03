636f4907658ce11193ffb98d7d3b44dfb1064157
harlan <harlan@verthein.us>
2019-05-02 23:08:48 -0700

    Testing styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import ".style.css";
+import React from "react";
+
+
 <html>
     <h1>
         <center>We know your schedule is busy. We'll make it so it doesn't have to be.</center>
@@ -5,9 +9,9 @@
 
     <body>
         <center>
-            <p>Do your part time jobs make your day harder to manage?<br />
-            Click below to learn how we can simplify your day and help<br />
-            you build a schedule that fits your personal needs and preferences.
+            <p>Do your part time jobs make your day harder to manage than you want?<br />
+            Click below to learn how we can simplify your day and help you build a<br />
+            schedule that fits your personal needs and preferences, saving you time.
             </p>
         </center>
 
