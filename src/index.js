460629a9d0d9b941ee199957972cd4b46dcd6b93
harlan <harlan@verthein.us>
2019-05-02 23:27:41 -0700

    Testing styles

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import "./style.css";
 import React from "react";
 
 const IndexPage = () => (
+    <body>
     <div>
     <h1>
         <center>We know your schedule is busy. We'll make it so it doesn't have to be.</center>
     </h1>
-
-    <body>
         <center>
             <p>Do your part time jobs make your day harder to manage than you want?<br />
             Click below to learn how we can simplify your day and help you build a<br />
             schedule that fits your personal needs and preferences, saving you time.
             </p>
         </center>
-    </body>
+
     </div>
+    </body>
 )
 
 export default IndexPage
